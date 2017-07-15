cbb6f6dc16a33c139c1243c7836d7ea8133bf665
Darin Kadrioski <darin@nebiru.com>
2017-07-14 17:31:19 -0700

    refinements

--- src/views/NotFound404.js
+++ src/views/NotFound404.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const NotFound404 = ({ location }) =>
   (<div className="not-found-404">
